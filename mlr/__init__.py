561dc49dfe95a57a910ab7dd02da2d6c3479931d
Lisa <lisa@system76-pc.localdomain>
2019-05-15 23:11:01 -0400

    correct kfold code based on new sklearn

--- mlr/__init__.py
+++ mlr/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sklearn.model_selection as skms
-
+import matplotlib.pyplot as plt
 
 def mlr(x_of_trainingset, y_actual):
     columnnames = list(x_of_trainingset.columns.values)
@@ -23,15 +23,12 @@ def mlr(x_of_trainingset, y_actual):
 
 
 def mlrr(x_of_trainingset, y_actual):
+    #usethis
     npones = np.ones(len(x_of_trainingset), float)
-
     A_sl = x_of_trainingset
     A = np.column_stack([A_sl, npones])
-
     lstsq, residuals, rank, something = np.linalg.lstsq(A, y_actual)
-    return lstsq
-
-
+    return lstsq, residuals
 def pmlr(x_of_trainingset, y_actual):
     npones = np.ones(len(y_actual), float)
     A = np.column_stack([x_of_trainingset, npones])
@@ -43,72 +40,40 @@ def kfoldmlr(xi, yi, **kwargs):
     '''gives the y-hats for a q2LOO calculation'''
     x = xi.values
     y = yi.values
-
+    nfolds=kwargs["nfolds"]
+    mean=kwargs["mean"]
     kf = skms.KFold(n_splits=nfolds)  # indices=None, shuffle=False, random_state=None)
     y_hats = []
     print(kf)
-    for train_index, test_index in kf.get_n_splits(x):
+    for train_index, test_index in kf.split(x):
         x_train, x_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        coefficients = mlrr(x_train, y_train)
-        # x_train=x_train
-        # y_train=y_train
-        yhat = coefficients[-1]
-
-        for index in range(x_test.size):
-            slopetimesx = x_test[0][index] * coefficients[index]
-            yhat = yhat + slopetimesx
-        y_hats.append(float(yhat))
-
-    # cleanyhats=[]
+        coefficients = mlrr(x_train, y_train)[0]
+        resids = mlrr(x_train, y_train)[1]
+        y_hats.append(resids)
     # for e in y_hats:
     #    cleanyhats.append(float(e))
     stack = np.asarray(y_hats)
-    return stack
+    if mean==True:
+        return np.mean(stack)
+    else:
+        return stack
 
 
-def q2loo_mlr(x, y):
-    '''calculates q2loo of a linear regression of x and y where both x and y are 1-d'''
-    yhats = kfoldmlr(x, y, len(x))
-    PRESS = np.sum((y - yhats) ** 2)
-    y_mean = np.mean(y)
-    TSS = np.sum((y - y_mean) ** 2)
-    # TSS=sum((y-np.mean(y))**2)
-    r2cv = (TSS - PRESS) * (TSS ** (-1))  # 1-(PRESS/TSS)
-    return r2cv
-
-
-def kfoldmlr2(xi, yi, nfolds):
+def kfoldmlrplot(xi, yi, **kwargs):
+    '''gives the y-hats for a q2LOO calculation'''
     x = xi.values
     y = yi.values
-
-    kf = skms.KFold(len(y), n_splits=nfolds)  # indices=None, shuffle=False, random_state=None)
+    nfolds=kwargs["nfolds"]
+    kf = skms.KFold(n_splits=nfolds)  # indices=None, shuffle=False, random_state=None)
     y_hats = []
-    y_tests = []
-    for train_index, test_index in kf:
+    print(kf)
+    for train_index, test_index in kf.split(x):
         x_train, x_test = x[train_index], x[test_index]
-        y_train = y[train_index]
-        coefficients = pmlr(x_train, y_train)
-        yhat = np.dot(x_test, coefficients[:-1]) + coefficients[-1]
-        y_test = y[test_index]
-        y_tests.append(np.mean(y_test))
-
-        y_hat = np.mean(yhat)
-        y_hats.append(y_hat)
-    y_tests = np.asarray(y_tests)
-    stack = np.asarray(y_hats)
-    return stack, y_tests
-
-
-def q2lmo_mlr(x, y, kfolds=5):
-    '''calculates q2loo of a linear regression of x and y where both x and y are 1-d'''
-    if type(kfolds) != int:
-        raise TypeError
-    else:
-        yhats, ytest = kfoldmlr2(x, y, kfolds)
-        # ytest=kfoldmlr2(x,y,kfolds)[1]
-        PRESS = np.sum((ytest - yhats) ** 2)
-        y_mean = np.mean(ytest)
-        TSS = np.sum((ytest - y_mean) ** 2)
-        r2cv = 1 - (PRESS / TSS)
-    return r2cv
+        y_train, y_test = y[train_index], y[test_index]
+        coefficients = mlrr(x_train, y_train)[0]
+        resids = mlrr(x_train, y_train)[1]
+        plt.plot(x_train, y_train, 'o', label='Original data', markersize=5)
+        plt.plot(x_train, coefficients[0]*x_train + coefficients[1], 'r', label='Fitted line')
+        plt.legend()
+        plt.show()
